b131b0517bb97398bb9debf446421afc768feb19
yato <yato28zzzz@gmail.com>
2024-10-26 12:55:40 +0900

    アプリ画面の作成

---
+++ lib/pages/setting_page/setting_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("設定"),
+      ),
+      body: Center(
+        child: Text("設定画面"),
+      ),
+    );
+  }
+}
